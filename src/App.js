7fb43669588a8adbfcf2539f503a1ba9e5cc85c6
Zain Tariq Sahi <sahizain9@gmail.com>
2021-10-29 17:55:42 +0500

    first-commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React,{useState} from 'react';
+import AddUsers from './components/Users/AddUsers';
+import UserList from './components/Users/UsersList';
+
+
+function App() {
+  const [userList,setUserList]=useState([]);
+const addUserHandler=(uName,uAge)=>{
+  setUserList((prevList)=>{
+    return [...prevList,{name:uName,age:uAge,id:Math.random.toString()}]
+
+  })
+
+}
+  
+  return (
+    <div>
+      <AddUsers onAddUser={addUserHandler}/>
+      <UserList users={userList}/>
+
+    </div>
+  );
+}
+
+export default App;
